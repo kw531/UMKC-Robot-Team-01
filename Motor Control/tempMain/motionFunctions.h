59cd1366b7e05d110917dae5e8153da2b7678933
Kati Williams <katiwilliams531@gmail.com>
2018-03-25 07:30:14 -0500

    Commenting and fixing an data type issue
    Commented the functions for easier understanding, fixing the right turn's int angle to a float angle.  May have been why we had a weird issue.

--- Motor Control/tempMain/motionFunctions.h
+++ Motor Control/tempMain/motionFunctions.h
@@ -30,19 +30,19 @@ void IMUSetup() {
 
 void conveyorSetup() {
 
-  AFMSc.begin();  // create with the default frequency 1.6KHz
-
-  cnvyr1->setSpeed(150);
+  AFMSc.begin();  // Motorsheild setup for the conveyor
+  
+  cnvyr1->setSpeed(255);
   cnvyr1->run(FORWARD);
   cnvyr1->run(RELEASE);
-  cnvyr2->setSpeed(150);
+  cnvyr2->setSpeed(255);
   cnvyr2->run(FORWARD);
   cnvyr2->run(RELEASE);
 }
 
 void driversSetup() {
 
-  AFMSd.begin();  // create with the default frequency 1.6KHz
+  AFMSd.begin();  // Motorsheild setup for driving
 
   // Set the speed to start, from 0 (off) to 255 (max speed)
   int driveSpeed = 200;
@@ -62,17 +62,18 @@ void driversSetup() {
   leftMotorRear->run(RELEASE);
 }
 
-// encoder event for the interrupt call
 void leftEncoderEvent() {
+  // Left encoder interrupt
   leftCount++;
 }
 
-// encoder event for the interrupt call
 void rightEncoderEvent() {
+  // Right encoder interrupt
   rightCount++;
 }
 
 void motionSetup() {
+  // Calls all the setup functions for the systems
   Serial.begin(9600);           // set up Serial library at 9600 bps
   encoderSetup();
   IMUSetup();
@@ -81,14 +82,17 @@ void motionSetup() {
 }
 
 void runConveyor() {
+  // Turn on the Conveyor
+  int speed = 255 // Cannot be lower than this or conveyor doesn't move
   cnvyr1->run(FORWARD);
   cnvyr2->run(BACKWARD);
 
-  cnvyr1->setSpeed(255);
-  cnvyr2->setSpeed(255);
+  cnvyr1->setSpeed(speed);
+  cnvyr2->setSpeed(speed);
 }
 
 void forward (int dist) {
+  // Robot moves distance (inches) forward
   rightMotorFront->run(FORWARD);
   rightMotorRear->run(FORWARD);
   leftMotorFront->run(FORWARD);
@@ -100,6 +104,7 @@ void forward (int dist) {
 }
 
 void backward (int dist) {
+  // Robot moves distance (inches) backwards
   rightMotorFront->run(BACKWARD);
   rightMotorRear->run(BACKWARD);
   leftMotorFront->run(BACKWARD);
@@ -111,9 +116,10 @@ void backward (int dist) {
 }
 
 void moveRobot(int dist) {
-  // Reset the encoder count
+  // Move the robot in inches
   int driveSpeed = 150;
-  leftCount = 0;
+  
+  leftCount = 0; // Reset the encoder counts
   rightCount = 0;
   unsigned long ticks;
 
@@ -123,12 +129,14 @@ void moveRobot(int dist) {
     rightMotorRear->setSpeed(driveSpeed);
     leftMotorFront->setSpeed(driveSpeed);
     leftMotorRear->setSpeed(driveSpeed);
-  } while (((leftCount + rightCount) / 2) < ticks);
+  } while (((leftCount + rightCount) / 2) < ticks); // Average the encoder count
 }
 //
 void left(float angle) {
+  // Turns the robot left to an angle of degrees.
   // turning left = higher angle on IMU
-  IMUreset();
+  
+  IMUreset(); // Zero the IMU before turning
   float IMUangle = 0;
   int speed;
   rightMotorFront->run(BACKWARD);
@@ -136,27 +144,29 @@ void left(float angle) {
   leftMotorFront->run(FORWARD);
   leftMotorRear->run(FORWARD);
 
-  sensors_event_t event;
+  sensors_event_t event; // Get the angle from the IMU
   bno.getEvent(&event);
   IMUangle = event.orientation.x;
 
-  Serial.print("\tLeft turn X: ");
+  Serial.print("\tLeft turn X: "); // Issue tracking printout
   Serial.print(event.orientation.x, 2);
   Serial.println("");
+  
   while (IMUangle > 360.0 - angle || IMUangle < 3.0) {
-    sensors_event_t event;
+    //sensors_event_t event;
     bno.getEvent(&event);
-    IMUangle = event.orientation.x;
+    IMUangle = event.orientation.x; // Get new IMU angle
 
     if (IMUangle < 360.0 - (angle - (angle * .05))) {
+      // Reduce speed as we get closer to
       speed = 80;
-      rightMotorFront->setSpeed(100);
+      rightMotorFront->setSpeed(speed + 20); // Laggy motor, gets a boost
       rightMotorRear->setSpeed(speed);
       leftMotorFront->setSpeed(speed);
       leftMotorRear->setSpeed(speed);
     } else {
       speed = 150;
-      rightMotorFront->setSpeed(170);
+      rightMotorFront->setSpeed(speed + 20); // Laggy motor, gets a boost
       rightMotorRear->setSpeed(speed);
       leftMotorFront->setSpeed(speed);
       leftMotorRear->setSpeed(speed);
@@ -166,11 +176,13 @@ void left(float angle) {
 }
 
 
-void right(int angle) {
+void right(float angle) {
+  // Turns the robot right to a specific angle
   // Turning right = smaller angle on IMU
-  IMUreset();
+  IMUreset(); // Zero the IMU
   int IMUangle = 0;
   int speed;
+  
   rightMotorFront->run(FORWARD);
   rightMotorRear->run(FORWARD);
   leftMotorFront->run(BACKWARD);
@@ -178,14 +190,15 @@ void right(int angle) {
 
   sensors_event_t event;
   bno.getEvent(&event);
-  IMUangle = event.orientation.x;
+  IMUangle = event.orientation.x; // Get IMU angle
 
   while (IMUangle < (angle) || IMUangle >= 359) {
-    sensors_event_t event;
+    //sensors_event_t event;
     bno.getEvent(&event);
-    IMUangle = event.orientation.x;
+    IMUangle = event.orientation.x; // Get IMU angle
 
     if (IMUangle >= (angle - (angle * .1))) {
+      // Reduce speed as we get closer
       speed = 100;
       rightMotorFront->setSpeed(speed);
       rightMotorRear->setSpeed(speed);
@@ -206,16 +219,23 @@ void right(int angle) {
 
 
 void cleanUp() {
+  // RELEASE THE MOTORS
   rightMotorFront->run(RELEASE);
   rightMotorRear->run(RELEASE);
   leftMotorFront->run(RELEASE);
   leftMotorRear->run(RELEASE);
 }
 
+void conveyorCleanUp(){
+  // Not used currently, but preparation for turning off the conveyor when we don't need it.
+  cnvyr1->run(RELEASE);
+  cnvyr2->run(RELEASE);
+}
+
 void IMUreset() {
   // Resets the IMU so that the calibration is zero after a turn
   bno.begin();
-  delay(750);
+  delay(700);
 }
 
 #endif
