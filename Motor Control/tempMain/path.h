67422bbe15eb0309a675e6a884e99f4a9bf0ee26
Kati Williams <katiwilliams531@gmail.com>
2018-03-25 07:39:03 -0500

    Commented the path.h to explain the movement variables

--- Motor Control/tempMain/path.h
+++ Motor Control/tempMain/path.h
@@ -1,18 +1,25 @@
 #ifndef PATH_H
 #define PATH_H
-//#include <avr/pgmspace.h>
 #include "motionFunctions.h"
 
 void performMovement(char c, int n);
+
+// Each round has 2 variables.  round_PickupCode is the type of movement the robot needs to preform. 
+// This can be forward ('f'), backwards ('b'), left ('l'), and right ('r')
+// round_Pickup is the distance or angle for the robot move.  If it is a distance to move forward or backwards,
+// the value is expressed in inches. If it's a turning angle, it's degrees.
+
 char round1PickupCode[] = {'f', // travel to box 1
-                                                'r', 'f', 'l', 'f', 'l', 'f', 'l', 'f', // box 1
-                                                'r', 'f', // travel to box 3
-                                                'r' ,'f', 'r', 'f', 'r', 'f', 'l', 'f','r','f'}; // box 3
+                           'r', 'f', 'l', 'f', 'l', 'f', 'l', 'f', // box 1
+                           'r', 'f', // travel to box 3
+                           'r' ,'f', 'r', 'f', 'r', 'f', // box 3
+                           'l', 'f','r','f'}; // Travel to red square then to white square
                                                 
 int round1Pickup[] = {30, // travel to box 1 
-                                            90, 9, 92, 25, 93, 24, 90, 25, // box 1
-                                            45, 17, // travel to box 3 
-                                            127, 48, 85, 48, 85, 46 ,45, 24, 135, 40};  // box 3
+                      90, 9, 92, 25, 93, 24, 90, 25, // box 1
+                      45, 17, // travel to box 3 
+                      127, 48, 85, 48, 85, 46 , // box 3
+                      45, 24, 135, 40};  // Travel to red square then to white square
 
 void roundOne() {
   Serial.println(sizeof(round1PickupCode)); Serial.println("");
@@ -25,8 +32,7 @@ void roundOne() {
 }
 
 void performMovement(char c, int n) {
-    //Serial.println(c);
-    Serial.println(n);
+  // The case statement which sends the motion and the distance to the motors
   switch (c) {
     case 'f':
       Serial.println("FORWARD");
